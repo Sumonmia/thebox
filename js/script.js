2afebf5f7d22adba046bd8b0c1327b92dbc44e6b
Md. Sumon Mia <sumon.ruetbd@gmail.com>
2023-10-07 16:14:04 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// jquery plugins
+$(document).ready(function(){
+            // counter up
+            $('.ex_count').counterUp({
+                delay: 10,
+                time: 1000
+            });
+
+            // mixit up the box
+            var mixer = mixitup('.menu_img', {
+                animation: {
+                    duration: 300
+                }
+            });
+})
